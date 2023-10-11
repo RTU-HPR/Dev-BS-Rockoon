372bd785719a880171808ba8b112610cf9d11c3b
Martin Grinums <65511845+Helgrafs@users.noreply.github.com>
2023-10-11 18:35:33 +0300

    ESP NOW framework

---
+++ espNowReceiver/src/main.cpp
@@ -0,0 +1,36 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Arduino.h>
+
+typedef struct struct_message {
+    double lon;
+    double lat;
+    double alt;
+} struct_message;
+
+struct_message myData;
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  String data = String(myData.lat) + "," + String(myData.lon) + "," + String(myData.alt);
+ Serial.println(data);
+}
+ 
+void setup(){
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK) {
+  Serial.println("Error initializing ESP-NOW");
+  return;
+  }else{
+    Serial.println("Initialised ESP now");
+  }
+esp_now_register_recv_cb(OnDataRecv);
+
+}
+ 
+void loop(){
+
+}
+
+// // DC:54:75:D0:49:40
