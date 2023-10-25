318cb1e440373712c65dc62aebbc2f0b077539fc
mouniss <monika.vainina@gmail.com>
2023-10-25 19:26:06 +0300

    Testing Lora on BFC
    Different board and protocol then loraSender
    env:esp32s3box
    LoRa chip:SX1268
    LoRa libary: radiolib

---
+++ test-loRaSender-BFC/src/main.cpp
@@ -0,0 +1,95 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <iostream>
+#include <LoRa.h>
+#include <EEPROM.h>
+#include <RadioLib.h>
+#include "LoRa.cpp"
+//SX1272 radios = new Module(loraSS, loraDIO0, loraRST, loraDIO1);
+#define LoRa_MOSI 11
+#define LoRa_MISO 13
+#define LoRa_SCK 12
+
+#define LoRa_nss 10
+#define LoRa_dio1 38
+#define LoRa_nrst 47
+#define LoRa_busy 48
+
+
+
+String outgoing = "string";
+SX1268 radio = new Module(LoRa_nss, LoRa_dio1, LoRa_nrst, LoRa_busy);
+void setup(){
+  //lora::setup;
+  Serial.begin(115200);
+  	for (int i=1; i<5; i++){	
+	Serial.println("SETUP");
+	delay(1000);}
+  SPI.begin(LoRa_SCK, LoRa_MISO, LoRa_MOSI, LoRa_nss);
+ 
+  // initialize SX1262 with default settings
+  Serial.print(F("[SX1268] Initializing ... "));
+  int state = radio.begin(434.0F,125.0F,10U,5,0xFF,1,8U,0);
+  if (state == RADIOLIB_ERR_NONE)
+  {
+    Serial.println(F("success!"));
+  }
+  else
+  {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true);
+  }
+}
+void transmitter (String outgoing){
+Serial.print(F("[SX1268] Transmitting packet ... "));
+
+  int state = radio.transmit(outgoing, 0xFF);
+
+  if (state == RADIOLIB_ERR_NONE)
+  {
+    // the packet was successfully transmitted
+    Serial.println(F("success!"));
+  }
+  else if (state == RADIOLIB_ERR_PACKET_TOO_LONG)
+  {
+    // the supplied packet was longer than 256 bytes
+    Serial.println(F("too long!"));
+  }
+  else if (state == RADIOLIB_ERR_TX_TIMEOUT)
+  {
+    // timeout occured while transmitting packet
+    Serial.println(F("timeout!"));
+  }
+  else
+  {
+    // some other error occurred
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+  }
+}
+double longtitude = 56.946285;
+double latitude = 24.105078;
+int altitude = 100;
+// void send(String message){
+//         Serial.println("Transmitting:");
+//         int state = radios.transmit(message);
+//         if(state == RADIOLIB_ERR_NONE){
+//             Serial.println("success");
+//         }else if(state == RADIOLIB_ERR_PACKET_TOO_LONG){
+//             Serial.println("failed, packet too long");
+//         }else{
+//             Serial.println(state);
+//         }
+// }
+//rewrite using sandeep mistry's lib
+void loop() {
+  // String(latitude) + "," + String(longtitude) + "," + 
+  String data =String(altitude);
+  //lora::send(data);
+  //send(data);
+  transmitter(data);
+  altitude = altitude +1;
+  delay(1000);
+  }
