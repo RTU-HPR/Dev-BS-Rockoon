0d652c4e3335987f6a0b38a6518435d81dddc7e2
RJ-SenLV <69112249+RJ-SenLV@users.noreply.github.com>
2023-11-02 11:35:08 +0200

    Restructured project, added error checks
    Split into 3 files - main, wrapper and class.
    added checks for obvious mistakes in received data (WIP)

---
+++ LoRa+Angles+espNow/include/class.h
@@ -0,0 +1,210 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <RadioLib.h>
+
+#include "angleCalculations.cpp"
+
+#include <esp_now.h>
+#include <WiFi.h>
+
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+    Serial.print("\r\nLast Packet Send Status:\t");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+typedef struct struct_message {
+    int elev;
+    int azim;
+} struct_message;
+
+class rotatorAngles {
+    public:
+        uint8_t* broadcastAddress;
+        uint32_t LoRa_SCK;
+        uint32_t LoRa_MISO;
+        uint32_t LoRa_MOSI;
+        uint32_t LoRa_nss;
+
+        esp_now_peer_info_t peerInfo;
+        // Create a struct_message called myData
+        struct_message myData;
+
+        //Antenna location, ierakstīt reālo
+        double antennaLongitude = 24.15;
+        double antennaLatitude = 56.94;
+
+        //Raķetes starta vērtība, pagaidām nejauši izvēlēta.
+        double rocketLongitude = 56.94628;
+        double rocketLatitude = 24.10507;
+        double rocketAltitude = 100;
+        float RSSI = 0;
+
+        //Saglabā jaunās koordinātes kamēr kamēr jāpārbauda korektums
+        double rocketLongitudeNew = 0;
+        double rocketLatitudeNew = 0;
+        double rocketAltitudeNew = 0;
+
+        //Saglagā pagājušo reālo lokāciju backupam kļūdas gadījumā
+        double rocketLongitutePrev = 0;
+        double rocketLatitudePrev = 0;
+        double rocketAltitudePrev = 0;
+        int elevationPrev = 0;
+        int azimuthPrev = 0;
+        bool backupExists = false;  //Mainīgais tiek pārlikts uz true, kad augšā esošie prev mainīgie iegūst reālas koordinātes, nevis placeholderus
+
+        //Maksimālās pieļaujamās mainīgo starpības starp laika posmiem, tos pārsniedzot pieņem, ka kļūdaini dati.
+        double longitudeDelta = 5;
+        double latitudeDelta = 5;
+        double AltitudeDelta = 1000;
+
+        double rotationPrecision = 2; //Antenas precizitāte grādos, abu virzienu vajadzīgā rotācija tiek noapaļota līdz tuvākajam šī skaitļa reizinājumam
+
+        rotatorAngles(uint8_t ba[], uint32_t SCK, uint32_t MISO, uint32_t  MOSI, uint32_t nss) { 
+            broadcastAddress = ba; 
+            LoRa_SCK = SCK;
+            LoRa_MISO = MISO;
+            LoRa_MOSI = MOSI;
+            LoRa_nss = nss;
+        }
+        void setup(SX1262 radio) {
+            Serial.begin(115200);
+            setWifiStation();
+            initESPNOW();
+            registerAddPeer();
+            SPI.begin(LoRa_SCK, LoRa_MISO, LoRa_MOSI, LoRa_nss);
+            initRadio(radio);
+
+            //TODO 
+            //setAntennaLocation();
+            //setRocketInitLoc();
+        }
+        void loop(SX1262 radio) {
+            receiveLocation(radio, true);
+            // if (checkNewLocation()) { //TODO, sakārtot kļūdu apstrādi, lai nevar iestrēgt.
+            //     updateLocation();
+            // }
+            updateLocation();
+            int elevation = calculateElevAngle(rocketLatitude, rocketLongitude, antennaLatitude, antennaLongitude, rocketAltitude, rotationPrecision);  //Radiānos
+            int azimuth = calculateAzimuth(rocketLatitude, rocketLongitude, antennaLatitude, antennaLongitude, rotationPrecision);
+            
+            debugPrintAngles(elevation, azimuth);
+            backup(elevation, azimuth);
+            send(broadcastAddress, elevation, azimuth);
+
+            delay(1000);
+        }
+
+    private:
+        void setWifiStation() {
+            // Set device as a Wi-Fi Station
+            WiFi.mode(WIFI_STA);
+        }
+        void initESPNOW(){
+            // Init ESP-NOW
+            if (esp_now_init() != ESP_OK) {
+                Serial.println("Error initializing ESP-NOW");
+                return;
+            }
+            // Once ESPNow is successfully Init, we will register for Send CB to
+            // get the status of Trasnmitted packet
+            esp_now_register_send_cb(OnDataSent);
+        }
+        void registerAddPeer() {
+            // Register peer
+            memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+            peerInfo.channel = 0;  
+            peerInfo.encrypt = false;
+            // Add peer        
+            if (esp_now_add_peer(&peerInfo) != ESP_OK){
+                Serial.println("Failed to add peer");
+                return;
+            }
+        }
+        void initRadio(SX1262 radio) {        
+            // initialize SX1262 with default settings
+            Serial.print(F("[SX1262] Initializing ... "));
+            int state = radio.begin(434.0F, 125.0F, 10U, 5U, 0xFF);
+            if (state == RADIOLIB_ERR_NONE)
+            {
+                Serial.println(F("success!"));
+            }
+            else
+            {
+                Serial.print(F("failed, code "));
+                Serial.println(state);
+                while (true);
+            }
+        }
+        void receiveLocation(SX1262 radio, bool debug = false){
+            String received;
+                int state = radio.receive(received);
+                if(state == RADIOLIB_ERR_NONE){
+                //TODO saglabāt tīros datus (received) SD vai tml., iespējams vajag piefiksēt arī kļūdas (state)
+                parseLocation(received, debug);
+                RSSI = radio.getRSSI();
+            }else{
+                Serial.println(state);
+            }
+        }
+        void parseLocation(String data, bool debug){
+            if (debug) {Serial.println("rawData: " + String(data));}
+
+            //String apstrāde
+            String delimiter = ",";
+            rocketLatitudeNew = data.substring(0, data.indexOf(delimiter)).toDouble();
+            data = data.substring(data.indexOf(delimiter)+1);
+            rocketLongitudeNew = data.substring(0, data.indexOf(delimiter)).toDouble();
+            rocketAltitudeNew = data.substring(data.indexOf(delimiter)+1).toDouble();
+            if (debug) {Serial.println("Lat: " + String(rocketLatitudeNew) + "Lng: " + String(rocketLongitudeNew) + "Alt: " + String(rocketAltitudeNew));}
+
+        }
+        bool checkNewLocation() {
+            // Ja dati ir inherently nepareizi, piem, negatīvas koordinātes
+            if (rocketLatitudeNew < 0 || rocketLatitudeNew > 180) {return false;}
+            if (rocketLongitudeNew < 0 || rocketLongitudeNew > 360) {return false;}
+
+            // Ja kādas vērtības izmaiņas pārsniedz izvēlētās robežvērtības (piem. pārleca vairākus koordināšu grādus)
+            if (abs(rocketAltitude - rocketAltitudeNew) > AltitudeDelta) {return false;}
+            if (abs(rocketLatitude - rocketLatitudeNew) > latitudeDelta) {return false;}
+            if (abs(rocketLongitude - rocketLongitudeNew) > longitudeDelta) {return false;}
+
+            if (rocketAltitudeNew < 0) {rocketAltitude = 0;}
+
+            return true;
+        }
+        void updateLocation() {
+            // Globālajos mainīgajos ieraksta jaunās vērtības
+            rocketLatitude = rocketLatitudeNew;
+            rocketLongitude = rocketLongitudeNew;
+            rocketAltitude = rocketAltitudeNew;
+        }
+        void debugPrintAngles(int elevation, int azimuth){
+            Serial.println("elevation: " + String(elevation) + " azimuth:" + String(azimuth) + " RSSI:" + String(RSSI));
+        }
+        void backup(int elevation, int azimuth){
+            //TODO saglabāt visus šos mainīgos SD vai tml.
+            elevationPrev = elevation;
+            azimuthPrev = azimuth;
+            rocketLongitutePrev = rocketLongitude;
+            rocketLatitudePrev = rocketLatitude;
+            rocketAltitudePrev = rocketAltitude;
+            backupExists = true; //Lai ir zināms, ka ir backups, nevis placeholder vērtības
+        }
+        void send(uint8_t broadcastAddress[], int elevation, int azimuth) { 
+            // Set values to send
+            myData.elev = elevation;
+            myData.azim = azimuth;
+
+            // Send message via ESP-NOW
+            esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+
+            if (result == ESP_OK) {
+                Serial.println("Sent with success");
+            }
+            else {
+                Serial.println("Error sending the data");
+            }
+        }
+};
--- LoRa+Angles+espNow/src/main.cpp
+++ LoRa+Angles+espNow/src/main.cpp
@@ -1,146 +1,15 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <SPI.h>
 #include <RadioLib.h>
+#include "class.h"
+#include "wrapper.h"
 
-#include "angleCalculations.cpp"
-
-#include <esp_now.h>
-#include <WiFi.h>
-
-#define LoRa_MOSI 10
-#define LoRa_MISO 11
-#define LoRa_SCK 9
-
-#define LoRa_nss 8
-#define LoRa_dio1 14
-#define LoRa_nrst 12
-#define LoRa_busy 13
-
-// REPLACE WITH YOUR RECEIVER MAC Address
-uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
-// Structure example to send data
-// Must match the receiver structure
-typedef struct struct_message {
-    int elev;
-    int azim;
-} struct_message;
-
-// Create a struct_message called myData
-struct_message myData;
-
-esp_now_peer_info_t peerInfo;
-
-// callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-}
-
+rotatorAngles rotAngle = rotatorAngles(broadcastAddress, LoRa_SCK, LoRa_MISO, LoRa_MOSI, LoRa_nss);
 SX1262 radio = new Module(LoRa_nss, LoRa_dio1, LoRa_nrst, LoRa_busy);
+
 void setup(){
- // lora::setup;
-  Serial.begin(115200);
-  // Set device as a Wi-Fi Station
-  WiFi.mode(WIFI_STA);
-
-  // Init ESP-NOW
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
-  esp_now_register_send_cb(OnDataSent);
-  
-  // Register peer
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;  
-  peerInfo.encrypt = false;
-  
-  // Add peer        
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
-    return;
-  }
-  SPI.begin(LoRa_SCK, LoRa_MISO, LoRa_MOSI, LoRa_nss);
-
-  // initialize SX1262 with default settings
-  Serial.print(F("[SX1262] Initializing ... "));
-  int state = radio.begin();
-  if (state == RADIOLIB_ERR_NONE)
-  {
-    Serial.println(F("success!"));
-  }
-  else
-  {
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-    while (true);
-  }
-  
-}
-//Antenna location
-double antennaLongitude = 24.15;
-double antennaLatitude = 56.94;
-
-double rocketLongitude = 56.94628;
-double rocketLatitude = 24.10507;
-double rocketAltitude = 100;
-float RSSI = 0;
-
-int i = 5;
-
-
-
-double rotationPrecision = 5; //Antenas precizitāte grādos
-
-void getLocation(String data){
-  Serial.println(data);
-  String delimiter = ",";
-  rocketLatitude = data.substring(0, data.indexOf(delimiter)).toDouble();
-  data = data.substring(data.indexOf(delimiter)+1);
-  rocketLongitude = data.substring(0, data.indexOf(delimiter)).toDouble();
-  rocketAltitude = data.substring(data.indexOf(delimiter)+1).toDouble();
-}
-
-void receive(){
-  String received;
-   int state = radio.receive(received);
-   if(state == RADIOLIB_ERR_NONE){
-     getLocation(received);
-     RSSI = radio.getRSSI();
-   }else{
-     Serial.println(state);
-   }
+  rotAngle.setup(radio);
 }
 
 void loop() {
-
-  receive();
-  int elevation = calculateElevAngle(rocketLatitude, rocketLongitude, antennaLatitude, antennaLongitude, rocketAltitude, rotationPrecision);  //Radiānos
-  int azimuth = calculateAzimuth(rocketLatitude, rocketLongitude, antennaLatitude, antennaLongitude, rotationPrecision);
-  Serial.println("elevation: " + String(elevation) + " azimuth:" + String(azimuth) + " RSSI:" + String(RSSI));
-  
-  if (i==10){
-    i = 0;
-      // Set values to send
-    myData.elev = elevation;
-    myData.azim = azimuth;
-    
-    // Send message via ESP-NOW
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-    
-    if (result == ESP_OK) {
-      Serial.println("Sent with success");
-    }
-    else {
-      Serial.println("Error sending the data");
-    }
-  }
-  i++;
-
-  delay(1000);
+  rotAngle.loop(radio);
 }
