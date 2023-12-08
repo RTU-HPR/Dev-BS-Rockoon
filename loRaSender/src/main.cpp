e96bf6d5639bd9eadd22e09d545c5e913c880012
DaniielZ <34136852+DaniielZ@users.noreply.github.com>
2023-12-08 17:51:11 +0200

    update dto new lib

--- loRaSender/src/main.cpp
+++ loRaSender/src/main.cpp
@@ -1,89 +1,105 @@
 #include <Arduino.h>
-#include <Wire.h>
 #include <SPI.h>
-#include <iostream>
-#include <LoRa.h>
-#include <EEPROM.h>
-#include <RadioLib.h>
-#include "LoRa.cpp"
-//SX1272 radios = new Module(loraSS, loraDIO0, loraRST, loraDIO1);
-#define LoRa_MOSI 10
-#define LoRa_MISO 11
-#define LoRa_SCK 9
-#define LoRa_nss 8
-#define LoRa_dio1 14
-#define LoRa_nrst 12
-#define LoRa_busy 13
-String outgoing = "string";
-SX1262 radio = new Module(LoRa_nss, LoRa_dio1, LoRa_nrst, LoRa_busy);
-void setup(){
-  //lora::setup;
-  Serial.begin(115200);
-  SPI.begin(LoRa_SCK, LoRa_MISO, LoRa_MOSI, LoRa_nss);
+#include "RadioLib_wrapper.h"
 
-  // initialize SX1262 with default settings
-  Serial.print(F("[SX1262] Initializing ... "));
-  int state = radio.begin();
-  if (state == RADIOLIB_ERR_NONE)
-  {
-    Serial.println(F("success!"));
-  }
-  else
-  {
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-    while (true);
-  }
+// Pins for the SPI bus, that the radio module uses
+const int SPI_RX = 11; // MISO
+const int SPI_TX = 10; // MOSI
+const int SPI_SCK = 9;
+
+// Used radio module series
+// Our commonly used LoRa module and its series:
+//  * RFM96 - Module is SX1272 and family is SX127X
+//  * E22-400 - Module is SX1268 and family is SX126X
+#define radio_module SX1262
+#define radio_module_family SX126X
+
+// RX/TX switching used by the radio module
+//  * DIO2 - If RX/TX enable is controlled by radio chip using DIO2
+//  * GPIO - If RX/TX enable controlled by micro controller GPIO pins (if this is set define RX_enable TX_enable gpio pins)
+//  * DISABLED  - If not using either, set to this
+// DIO2 and GPIO currently only set up for SX126X LoRa
+#define radio_module_rf_switching DISABLED
+
+// Radio module config
+RadioLib_Wrapper<radio_module>::RADIO_CONFIG radio_config{
+    .FREQUENCY = 434, // Frequency
+    .CS = 8,          // Chip select
+    .DIO0 = 13,       // Busy
+    .DIO1 = 14,       // Interrupt action
+    .FAMILY = RadioLib_Wrapper<radio_module>::RADIO_CONFIG::CHIP_FAMILY::radio_module_family,
+    .rf_switching = RadioLib_Wrapper<radio_module>::RADIO_CONFIG::RF_SWITCHING::radio_module_rf_switching,
+    // If using GPIO pins for RX/TX switching, define pins used for RX and TX control
+    .RX_ENABLE = -1,
+    .TX_ENABLE = -1,
+    .RESET = 12,
+    .SYNC_WORD = 0xF4,
+    .TXPOWER = 14,
+    .SPREADING = 10,
+    .CODING_RATE = 7,
+    .SIGNAL_BW = 125,
+    .SPI_BUS = &SPI // SPI bus used by radio
+};
+
+// Create radio object
+RadioLib_Wrapper<radio_module> radio = RadioLib_Wrapper<radio_module>(nullptr);
+
+// Ping pong
+bool should_transmit = true;
+int message_index = 1;
+int WAIT_FOR_RECEIVE = 5000;
+unsigned long last_transmit_time = 0;
+
+void setup()
+{
+    // Configure and begin SPI bus
+    radio_config.SPI_BUS->begin(SPI_SCK, SPI_RX, SPI_TX);
+
+    // Configure radio module
+    if (!radio.begin(radio_config))
+    {
+        while (true)
+        {
+            Serial.println("Configuring LoRa failed");
+            delay(5000);
+        }
+    }
+
+    // If required a test message can be transmitted
+    // radio.test_transmit();
 }
-void transmitter (String outgoing){
-Serial.print(F("[SX1262] Transmitting packet ... "));
 
-  int state = radio.transmit(outgoing);
+void loop()
+{
+    // Variables to store the received data
+    String msg = "";
+    float rssi = 0;
+    float snr = 0;
 
-  if (state == RADIOLIB_ERR_NONE)
-  {
-    // the packet was successfully transmitted
-    Serial.println(F("success!"));
-  }
-  else if (state == RADIOLIB_ERR_PACKET_TOO_LONG)
-  {
-    // the supplied packet was longer than 256 bytes
-    Serial.println(F("too long!"));
-  }
-  else if (state == RADIOLIB_ERR_TX_TIMEOUT)
-  {
-    // timeout occured while transmitting packet
-    Serial.println(F("timeout!"));
-  }
-  else
-  {
-    // some other error occurred
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-  }
+    // Check if anything has been received
+    if (radio.receive(msg, rssi, snr))
+    {
+        // Check if checksum matches
+        if (radio.check_checksum(msg))
+        {
+            Serial.println("LoRa received: " + msg + " | RSSI: " + rssi + "| SNR: " + snr);
+        }
+        else
+        {
+            Serial.println("LoRa received with checksum fail: " + msg + " | RSSI: " + rssi + "| SNR: " + snr);
+        }
+        should_transmit = true;
+    }
+
+    // If nothing has been received in the defined time period, transmit a message
+    if (should_transmit || millis() - last_transmit_time > WAIT_FOR_RECEIVE)
+    {
+        String tx_message = "Ping pong message " + String(message_index);
+        radio.add_checksum(tx_message);
+        radio.transmit(tx_message);
+
+        message_index++;
+        should_transmit = false;
+        last_transmit_time = millis();
+    }
 }
-double longtitude = 56.9527230;
-double latitude = 24.0809493;
-int altitude = 100;
-// void send(String message){
-//         Serial.println("Transmitting:");
-//         int state = radios.transmit(message);
-//         if(state == RADIOLIB_ERR_NONE){
-//             Serial.println("success");
-//         }else if(state == RADIOLIB_ERR_PACKET_TOO_LONG){
-//             Serial.println("failed, packet too long");
-//         }else{
-//             Serial.println(state);
-//         }
-// }
-//rewrite using sandeep mistry's lib
-void loop() {
-  
-  String data = String(longtitude) + "," + String(latitude) + "," + String(altitude);
-  Serial.println(data);
-  //lora::send(data);
-  //send(data);
-  transmitter(data);
-  altitude = altitude +100;
-  delay(1000);
-  }
